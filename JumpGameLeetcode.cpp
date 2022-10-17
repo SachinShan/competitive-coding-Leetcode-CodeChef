afffa21f855f8ce4ab30590922abadce2d54f3dd
Sachin Shan <krsachin9696@gmail.com>
2022-10-17 23:45:23 +0530

    Jump Game Leetcode

---
+++ JumpGameLeetcode.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        // vector<int> dp;
+
+        int n = nums.size();
+        int temp = 0, i = 0;
+
+        while (temp != n - 1 || temp < n - 1)
+        {
+            if (nums[i] == 0)
+            {
+                break;
+            }
+            temp = temp + nums[i];
+            i = i + nums[i];
+        }
+
+        if (temp = n - 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
