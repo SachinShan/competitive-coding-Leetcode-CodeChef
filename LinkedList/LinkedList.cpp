42021247b4dd4258df938ecc18de52885c46b7e6
Sachin Shan <krsachin9696@gmail.com>
2022-10-20 00:00:04 +0530

    mmm

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -14,9 +14,56 @@ public:
     }
 };
 
+void InsertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void InsertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+
+void print(Node *&head)
+{
+
+    Node *tem = head;
+    while (tem != NULL)
+    {
+        cout << tem->data << " ";
+        tem = tem->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
     Node *node1 = new Node(10);
     cout << node1->data << endl;
     cout << node1->next << endl;
+
+    Node *head = node1;
+    Node *tail = node1;
+
+    InsertAtHead(head, 20);
+    print(head);
+
+    InsertAtHead(head, 30);
+    print(head);
+
+    InsertAtHead(head, 40);
+    print(head);
+
+    InsertAtTail(tail, 50);
+    print(head);
+
+    InsertAtTail(tail, 60);
+    print(head);
+
+    InsertAtTail(tail, 70);
+    print(head);
 }
