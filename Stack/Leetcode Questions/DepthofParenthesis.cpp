610b4ab92b686ae5bc796d322644faa101613682
Sachin Shan <krsachin9696@gmail.com>
2022-11-17 19:10:25 +0530

    Stack Leetcode

---
+++ Stack/Leetcode Questions/DepthofParenthesis.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maxDepth(string s)
+    {
+        int ans = 0, count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(')
+            {
+                ans = max(ans, ++count);
+            }
+            else if (s[i] == ')')
+            {
+                count--;
+            }
+        }
+        return ans;
+    }
+};
