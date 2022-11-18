e5d0c17abd90a8ff1689939a53bf8164d08a948f
Sachin Shan <krsachin9696@gmail.com>
2022-11-19 04:49:02 +0530

    Leetcode Tree

---
+++ Tree/Leetcode Questions/SameTree.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL && q == NULL)
+        {
+            return true;
+        }
+        if (p == NULL || q == NULL)
+        {
+            return false;
+        }
+        if (p->val != q->val)
+        {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+// 51/60 testcase passed
+// class Solution {
+// public:
+//     vector<int> ans1;
+//     vector<int> ans2;
+//     void inorder(TreeNode *root, vector<int>& ans){
+//             if(root == NULL){
+//             return;
+//         }
+//         inorder(root->left, ans);
+//         ans.push_back(root->val);
+//         inorder(root->right, ans);
+//     }
+
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         inorder(p, ans1);
+//         inorder(q, ans2);
+//         int flag = 1;
+//         if(ans1.size() == ans2.size()){
+//             for(int i=0; i<ans1.size(); i++){
+//                 if(ans1[i] != ans2[i])
+//                     flag = 0;
+//                     break;
+//             }
+//         }
+//         else{
+//             flag = 0;
+//         }
+//         return flag;
+//     }
+// };
