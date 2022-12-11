339aee79fae4d7e61236cd8aad0d26e08c447c7d
Sachin Shan <krsachin9696@gmail.com>
2022-12-11 20:15:48 +0530

    leetcode Tree

---
+++ LeetCode Contest/DeleteGreatestNo.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int deleteGreatestValue(vector<vector<int>> &grid)
+    {
+        int sum = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            sort(grid[i].begin(), grid[i].end());
+        }
+
+        for (int i = 0; i < grid[0].size(); i++)
+        {
+            int m = 0;
+            for (int j = 0; j < grid.size(); j++)
+            {
+                m = max(m, grid[j][i]);
+            }
+            sum = sum + m;
+        }
+        return sum;
+    }
+};
