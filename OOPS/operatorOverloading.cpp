e2d18d0251a5d256fc85397e881a1f2b59e743e0
Sachin Shan <krsachin9696@gmail.com>
2023-01-04 10:04:53 +0530

    OOPS basic concepts

--- OOPS/operatorOverloading.cpp
+++ OOPS/operatorOverloading.cpp
@@ -1,15 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Complex {
+class Complex
+{
 private:
 	int real, imag;
+
 public:
-	Complex(int r = 0, int i = 0) {real = r; imag = i;}
-	
+	Complex(int r = 0, int i = 0)
+	{
+		real = r;
+		imag = i;
+	}
+
 	// This is automatically called when '+' is used with
 	// between two Complex objects
-	Complex operator + (Complex const &obj) {
+	Complex operator+(Complex const &obj)
+	{
 		Complex res;
 		res.real = real + obj.real;
 		res.imag = imag + obj.imag;
