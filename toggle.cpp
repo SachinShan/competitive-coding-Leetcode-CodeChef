0fea36ad8efee13c4ab692890e83e4e545ef462b
Sachin Shan <krsachin9696@gmail.com>
2023-01-07 20:18:05 +0530

    Leetcode Array

--- toggle.cpp
+++ toggle.cpp
@@ -2,28 +2,28 @@ int shuru = 0, abhi = 0;
 int jada = 0;
 int ginti = 0;
 // using sliding window
-while (abhi < arr.length)
+while (abhi < arr.length())
 {
     if (arr[abhi] == 1)
     {
         ginti++;
-        jada = Math.jada(jada, ginti);
+        jada = max(jada, ginti);
         abhi++;
     }
     else
     {
-        while (abhi < arr.length && arr[abhi] == 0)
+        while (abhi < arr.length() && arr[abhi] == 0)
         {
             ginti++;
             abhi++;
-            jada = Math.jada(jada, ginti);
+            jada = max(jada, ginti);
         }
         shuru = abhi;
-        while (abhi < arr.length && arr[abhi] == 1)
+        while (abhi < arr.length() && arr[abhi] == 1)
         {
             ginti++;
             abhi++;
-            jada = Math.jada(jada, ginti);
+            jada = max(jada, ginti);
         }
         abhi = shuru;
         ginti = 0;
