a18bb105d18ed22f986c53a2ef57680d81fe68de
Sachin Shan <krsachin9696@gmail.com>
2023-02-10 15:33:01 +0530

    heap

---
+++ Leetcode Questions/Heap-Priority_Queue/LastStoneWeight.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    int lastStoneWeight(vector<int> &stones)
+    {
+        priority_queue<int> pq;
+
+        for (int i = 0; i < stones.size(); i++)
+        {
+            pq.push(stones[i]);
+        }
+
+        while (pq.size() > 1)
+        {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            if (a == b)
+            {
+                if (pq.size() == 0)
+                {
+                    return 0;
+                }
+                else
+                    continue;
+            }
+            else
+            {
+                int c = a - b;
+                pq.push(c);
+            }
+        }
+        return pq.top();
+    }
+};
