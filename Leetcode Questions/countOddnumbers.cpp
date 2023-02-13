d58857dfbd477ecc6d6da8f6d58d25bdbe1cc993
Sachin Shan <krsachin9696@gmail.com>
2023-02-13 10:01:19 +0530

    Math

---
+++ Leetcode Questions/countOddnumbers.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+
+        return ((high + 1) / 2) - (low) / 2;
+    }
+};
+
+// class Solution {
+// public:
+//     int countOdds(int low, int high) {
+//         int count = 0;
+
+//         for(int i=low; i<=high; i++){
+//             if(i%2 != 0){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
