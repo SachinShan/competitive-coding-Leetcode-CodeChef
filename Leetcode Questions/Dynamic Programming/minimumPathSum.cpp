54529cb8da521ffa2f34035ae5740d6f0eecb5a8
Sachin Shan <krsachin9696@gmail.com>
2023-02-26 20:40:37 +0530

    Dynamic Programming

--- Leetcode Questions/Dynamic Programming/minimumPathSum.cpp
+++ Leetcode Questions/Dynamic Programming/minimumPathSum.cpp
@@ -1,3 +1,53 @@
+// Tabulation
+
+class Solution
+{
+public:
+    int minPathSum(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    dp[i][j] = grid[i][j];
+                }
+                else
+                {
+                    int up = grid[i][j];
+                    if (i > 0)
+                    {
+                        up += dp[i - 1][j];
+                    }
+                    else
+                    {
+                        up = INT_MAX;
+                    }
+
+                    int left = grid[i][j];
+                    if (j > 0)
+                    {
+                        left += dp[i][j - 1];
+                    }
+                    else
+                    {
+                        left = INT_MAX;
+                    }
+                    dp[i][j] = min(left, up);
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
+
+// Memoization
 class Solution
 {
 public:
