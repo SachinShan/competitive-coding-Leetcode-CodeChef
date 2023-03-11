a5ea7d09cbc25b3ac05455c56130159b5ed9c90d
krsachin9696 <krsachin9696@gmail.com>
2023-03-12 04:54:58 +0530

    Dynamic Programming

---
+++ Leetcode Questions/Dynamic Programming/DeleteOperationforTwoStrings.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int minDistance(string t1, string t2)
+    {
+        int n = t1.length();
+        int m = t2.length();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (t1[i - 1] == t2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return (n + m) - 2 * dp[n][m];
+    }
+};
