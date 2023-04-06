0b58aee1f96a07b64fdba2a7b713c4fae44ca039
krsachin9696 <krsachin9696@gmail.com>
2023-04-06 22:29:47 +0530

    Graph Traversal

--- Graph/BFS-Traversal.cpp
+++ Graph/BFS-Traversal.cpp
@@ -16,6 +16,37 @@ void printList(vector<vector<int>> &adj, int n)
     }
 }
 
+void bfsTraversal(vector<vector<int>> &adj)
+{
+    int n = adj.size();
+    queue<int> q;
+    vector<bool> visited(n + 1, false);
+    vector<int> bfs;
+
+    q.push(1);
+    visited[1] = true;
+
+    while (!q.empty())
+    {
+        int x = q.front();
+        bfs.push_back(x);
+        q.pop();
+        for (auto i : adj[x])
+        {
+            if (visited[i] == false)
+            {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+
+    for (auto j : bfs)
+    {
+        cout << j << " ";
+    }
+}
+
 int main()
 {
     int n, m;
@@ -23,19 +54,18 @@ int main()
     cout << "Here n is the number of nodes & m is the number of Edges";
     cin >> n >> m;
 
-    // vector<vector<int>> adj(n, vector<int>(n, 0));
     vector<vector<int>> adj(n + 1);
 
     for (int i = 0; i < m; i++)
     {
-        cout << "I is " << i << endl;
+
         int u, v;
         cin >> u >> v;
 
-        // adj[u][v] = 1;
-        // adj[v][u] = 1;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     printList(adj, n + 1);
+
+    bfsTraversal(adj);
 }
--- Graph/DFS-Traversal.cpp
+++ Graph/DFS-Traversal.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
+unordered_map<int, bool> visited;
 
 void printList(vector<vector<int>> &adj, int n)
 {
@@ -16,6 +17,24 @@ void printList(vector<vector<int>> &adj, int n)
     }
 }
 
+void dfs(vector<vector<int>> &adj, int node, vector<int> &ans)
+{
+    cout << "ye call hua hai"
+         << " " << node << endl;
+    visited[node] = true;
+    ans.push_back(node);
+
+    // traversing all the neighbours of the current node
+    for (auto i : adj[node])
+    {
+        cout << i << " ";
+        if (visited[i] == false)
+        {
+            dfs(adj, node, ans);
+        }
+    }
+}
+
 int main()
 {
     int n, m;
@@ -23,19 +42,26 @@ int main()
     cout << "Here n is the number of nodes & m is the number of Edges";
     cin >> n >> m;
 
-    // vector<vector<int>> adj(n, vector<int>(n, 0));
     vector<vector<int>> adj(n + 1);
 
     for (int i = 0; i < m; i++)
     {
-        cout << "I is " << i << endl;
+
         int u, v;
         cin >> u >> v;
 
-        // adj[u][v] = 1;
-        // adj[v][u] = 1;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     printList(adj, n + 1);
+
+    vector<int> ans; // vector to store the ans(dfs traversal)
+
+    dfs(adj, 1, ans);
+
+    cout << "DFS Traversal of the given Graph is: ";
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
 }
