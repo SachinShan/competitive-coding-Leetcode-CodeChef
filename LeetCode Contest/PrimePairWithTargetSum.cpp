103dd80b9e554397cb8381e0dbeed45e111435d9
krsachin9696 <krsachin9696@gmail.com>
2023-07-02 23:01:21 +0530

    02 July '23

---
+++ LeetCode Contest/PrimePairWithTargetSum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> findPrimePairs(int n) {
+//         vector<bool> temp(n + 1, true);
+//         temp[0] = false;
+//         temp[1] = false;
+
+//         for (int i = 2; i * i <= n; i++) {
+//             if (temp[i]) {
+//                 for (int j = i * i; j <= n; j += i) {
+//                     temp[j] = false;
+//                 }
+//             }
+//         }
+
+//         vector<vector<int>> P;
+//         for (int i = 2; i <= n / 2; i++) {
+//             if (temp[i] && temp[n - i]) {
+//                 P.push_back({i, n - i});
+//             }
+//         }
+
+//         return P;
+        
+        vector<bool> temp(n + 1, true);
+        temp[1] = false;
+        temp[0] = false;
+        
+
+        for (int i = 2; i * i <= n; i++) 
+            if (temp[i]) 
+                for (int j = i * i; j <= n; j += i) 
+                    temp[j] = false;
+                
+            
+        
+
+        vector<vector<int>> primePairs;
+        for (int i = 2; i <= n / 2; i++) 
+            if (temp[i] && temp[n - i]) 
+                primePairs.push_back({i, n - i});
+            
+        
+
+        return primePairs;
+    }
+};
---
+++ ja.js
@@ -0,0 +1,18 @@
+console.log("hellp")
+
+var a = 10;
+console.log(a);
+
+var a = 22;
+console.log(a);
+
+
+let d = 20;
+console.log(d);
+
+d = 22;
+console.log(d);
+
+let d = 33;
+console.log(d);
+
